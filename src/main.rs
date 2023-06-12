2a10ad1b3c358781db1d15996a1d176cc808b94e
tsc <tscasady@gmail.com>
2023-06-12 15:57:29 -0500

    serve example html

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -20,5 +21,12 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("{:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
