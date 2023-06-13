440e99f362dc7eae7348acc910e4cb34f4f43f9d
tsc <tscasady@gmail.com>
2023-06-13 12:17:36 -0500

    adds threadpool to handle multiple requests."

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,36 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| handle_connection(stream))
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (filename, status_line) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("hello.html", "HTTP/1.1 200 OK"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("hello.html", "HTTP/1.1 200 OK")
+        }
+        _ => ("404.html", "HTTP/1.1 404 NOT FOUND"),
+    };
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
